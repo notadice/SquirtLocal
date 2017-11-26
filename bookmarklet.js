4f66e9dc2c3f89f0f421d1b1beafe2870045c583
Zach Hamlin <zachhamlin6@gmail.com>
2017-11-26 16:59:39 -0600

    Changed link

--- bookmarklet.js
+++ bookmarklet.js
@@ -6,7 +6,7 @@ javascript:(function(){
     window.sq.userId = '--squirtUser--';
     window.sq.progressBarLocation = 'bottom'; /* 'bottom' | 'top' */
     s = document.createElement('script');
-    s.src = 'https://rawgit.com/notadice/SquirtLocal/master/squirt.js';
+    s.src = 'https://rawgit.com/zhamlin/SquirtLocal/master/squirt.js';
     s.s = window.location.search;
     s.idx = s.s.indexOf('sq-dev');
     document.body.appendChild(s);
--- squirt.js
+++ squirt.js
@@ -1,6 +1,6 @@
 var sq = window.sq;
 sq.version = '0.0.1';
-sq.host =  'https://rawgit.com/notadice/SquirtLocal/master/';
+sq.host =  'https://rawgit.com/zhamlin/SquirtLocal/master/';
 sq.cookies = {
 	loader: sq.host + 'cookies.html',
 	timeout: 3000,
